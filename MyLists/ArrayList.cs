9ae0df77f96615b5dc2f3afab832b06f7a90d975
Daria Shadrina <darinashadrina@yandex.ru>
2022-03-20 18:40:06 +0300

    Add methods "Write",  "UpSize and "Add"

--- MyLists/ArrayList.cs
+++ MyLists/ArrayList.cs
@@ -11,6 +11,51 @@ namespace MyLists
         public ArrayList()
         {
             _array = new int[10];
+            Length = 0;
+        }
+
+        public void Add(int value)
+        {
+            if (Length < _array.Length)
+            {
+                UpSize();
+            }
+            _array[Length] = value;
+            Length++;
+
+
+            //if (Length < _array.Length)
+            //{
+            //    _array[Length] = value;
+            //    Length++;
+            //}
+            //else
+            //{
+            //    UpSize();
+            //    _array[Length] = value;
+            //    Length++;
+            //}
+
+        }
+
+        public void Write()
+        {
+            for(int i=0; i < Length; i++)
+            {
+                Console.Write($"{_array[i] }");
+            }
+            Console.WriteLine();
+        } 
+
+        private void UpSize()
+        {
+            int newLength = (int)(_array.Length * 1.5d + 1);
+            int[] newArray = new int[newLength];
+            for (int i = 0; i < _array.Length; i++)
+            {
+                newArray[i] = _array[i];
+            }
+            _array = newArray;
         }
     }
 }
