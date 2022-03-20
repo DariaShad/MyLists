65acfd198ef088afdab5c1254468c31ff3b8d22c
Daria Shadrina <darinashadrina@yandex.ru>
2022-03-20 15:40:24 +0300

    Add properties at class

---
+++ MyLists/ArrayList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyLists
+{
+    public class ArrayList
+    {
+        public int Length { get; private set; }
+
+        private int[] _array;
+
+        public ArrayList()
+        {
+            _array = new int[10];
+        }
+    }
+}
--- MyLists/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MyLists
-{
-    public class Class1
-    {
-    }
-}
