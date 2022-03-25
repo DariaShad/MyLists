df1d79af4ed11858ac40efe56bca7f95429094d1
Daria Shadrina <dashadrina@edu.hse.ru>
2022-03-25 16:21:14 +0300

    add Test AddBeginning

--- MyLists.Tests/UnitTest1.cs
+++ MyLists.Tests/UnitTest1.cs
@@ -1,6 +1,9 @@
 using NUnit.Framework;
 using System.Collections;
+using MyLists;
+using MyLists.Tests.ArrayListTestSources;
 namespace MyLists.Tests
+
 {
     public class Tests
     {
@@ -15,28 +18,29 @@ namespace MyLists.Tests
             ArrayList arrayListExpected = new ArrayList(expected);
             Assert.AreEqual(arrayListExpected, arrayList);
         }
-    }
 
-    
-    [TestCaseSource(typeof(DeleteAllByValueTestSource))]
-    public void DeleteAllByValueTest(int value, ArrayList list, ArrayList expectedList, int expectedNumber)
-    {
-        int actualNumber = list.DeleteAllByValue(value);
-
-        ArrayList actualList = list;
-        Assert.AreEqual(expectedNumber, actualNumber);
-        Assert.AreEqual(expectedList, actualList);
-    }
-
-    public class DeleteAllByValueTestSource : IEnumerable
-    {
-        public IEnumerator GetEnumerator()
+        [TestCaseSource(typeof(DeleteAllByValueTestSource))]
+        public void DeleteAllByValueTest(int value, ArrayList list, ArrayList expectedList, int expectedNumber)
         {
-            int value = 3;
-            ArrayList list = new ArrayList(new int[] { 1, 2, 3, 4, 5 });
-            ArrayList expectedList = new ArrayList(new int[] { 1, 2, 4, 5 });
-            int expectedNumber = 1;
-            yield return new object[] {value, list, expectedList, expectedNumber };
+            int actualNumber = list.DeleteAllByValue(value);
+
+            ArrayList actualList = list;
+            Assert.AreEqual(expectedNumber, actualNumber);
+            Assert.AreEqual(expectedList, actualList);
+        }
+
+        [TestCaseSource(typeof(AddToBeginningTestSource))]
+        public void AddToBeginningTest(int value, ArrayList list, ArrayList expectedList)
+        {
+            list.AddToBeginning(value);
+            ArrayList actualList = list;
+            Assert.AreEqual(expectedList, actualList);
+        }
+        
+        [TestCaseSource(typeof(DeleteLastTestSource))]
+        public void DeleteLastTest(ArrayList list, ArrayList expectedList)
+        {
+            
         }
     }
 }
